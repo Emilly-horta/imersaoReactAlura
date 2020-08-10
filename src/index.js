2f0a0ee334c423cb9aecf359b395c5bd7e9370f2
parnaibaPyladies <parnaiba@pyladies.com>
2020-08-10 18:16:50 -0300

    Revert "Revert "Revert "fazendo atualizações pendentes"""
    This reverts commit 891c98c3d621fd0c919f7bb3a04940fdbe5fb878.

--- src/index.js
+++ src/index.js
@@ -1,25 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Home from './pages/Home';
-
-import {BrowserRouter, Switch, Route} from 'react-router-dom';
-import CadastroVideo from './pages/cadastro/Video'
-import CadastroCategoria from './pages/cadastro/categoria';
-
-const Pagina404 = () => (<div><h1>Pagina 404</h1></div>);
+import App from './App';
+import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <BrowserRouter>
-  <Switch>
-  <Route path="/" component={Home} exact />
-  <Route path="/cadastro/Video" component={CadastroVideo}/>
-  <Route path="/cadastro/categoria" component={CadastroCategoria}/>
-  <Route component={Pagina404}/>
-  </Switch>
-  </BrowserRouter>,
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
   document.getElementById('root')
 );
 
-
-
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
